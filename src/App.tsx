2075492d6ea80883fdcd7927cd5dd27478410859
ankojh <ankitkumarojha2@gmail.com>
2024-10-26 10:35:14 +0530

    react project setup

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import "./App.css";
+
+//todo: Error Boundaries
+//todo: Suspense Boundaries
+
+// 5 cards - Total users, Active Users, Total Streams, Revenue, Top Artist
+
+// 3 Charts - User Growth Chart, Revenue Distribution, Top Artists  - recharts
+
+// 1 table - Song Name, Artist, Date Streamed, Stream Count, User Id. - tanstack/react-table
+
+// Bonus - Drag to rearrange cards
+
+function App() {
+  return <></>;
+}
+
+export default App;
