a997decd51fa2a36be2c82cc19861d90d5858f2f
ankojh <ankitkumarojha2@gmail.com>
2024-10-27 00:59:48 +0530

    add tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    fontFamily: {
+      sans: ["Inter", "sans-serif"],
+      bold: ["Proza Libre", "sans-serif"],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
